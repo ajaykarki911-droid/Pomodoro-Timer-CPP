ec3087233305f340ad1442fb200d83176f50eb6b
ajaykarki911-droid <ajay.karki911@gmail.com>
2026-08-16 14:11:11 +0545

    Add CustomMode and mode selection menu

--- pomodoro.cpp
+++ pomodoro.cpp
@@ -49,10 +49,63 @@ public:
         cout << "Break time: " << breakTime << " minutes" << endl;
     }
 };
+// Custom mode
+
+class CustomMode : public TimerMode
+{
+public:
+    void setTime() override
+    {
+        cout << "\nEnter work time in minutes: ";
+        cin >> workTime;
+        cout << "Enter break time in minutes: ";
+        cin >> breakTime;
+    }
+
+    void showMode()
+    {
+        cout << "\nCustom mode selected" << endl;
+        cout << "Work time: " << workTime << " minutes" << endl;
+        cout << "Break time: " << breakTime << " minutes" << endl;
+    }
+};
 
 int main()
 {
+    int choice;
     cout << "*** Pomodoro Timer ***" << endl;
 
+    cout << "\n1. Default Mode (25/5)" << endl;
+    cout << "2. Custom Mode" << endl;
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    DefaultMode defaultMode;
+    CustomMode customMode;
+    TimerMode* mode;
+
+    if (choice == 1)
+    {
+        mode = &defaultMode;
+    }
+    else if (choice == 2)
+    {
+        mode = &customMode;
+    }
+    else
+    {
+        cout << "Invalid choice" << endl;
+        return 0;
+    }
+    mode->setTime();
+    if (choice == 1)
+    {
+        defaultMode.showMode();
+    }
+    else
+    {
+        customMode.showMode();
+    }
     return 0;
 }
